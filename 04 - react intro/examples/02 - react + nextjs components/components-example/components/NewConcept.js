639645d9bcc64e103ca54e2b758dd9fbef12c369
Robert Cao <rcao7@nait.ca>
2026-02-11 11:50:40 -0700

    react start

---
+++ 04 - react intro/examples/02 - react + nextjs components/components-example/components/NewConcept.js
@@ -0,0 +1,9 @@
+export default function NewConcept() {
+    // This is not a static component; we'll be passing input props 
+    // and rendering dynamically based on that data
+
+    return (
+        // I can write (inline javascript) (Thanks JSX!)
+        <p>In this class we are </p>
+    );
+}
